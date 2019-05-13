aaa4f1f6802ccbab14f527c27c30e536c040b6f1
quick-sort <quicksort@outlook.com>
2019-05-13 14:50:28 +0800

    0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pltshow',
-    version='0.1.0',
+    version='0.1.1',
     description='client for pltshow',
     long_description='client for pltshow to update dataset',
     author='quicksort',
